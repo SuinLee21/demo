f6255214a5c72813e2f7a685a3ebe11972ab893d
SuinLee21 <suin7473@naver.com>
2024-06-04 18:52:29 +0900

    13주차 이론

--- demo/src/main/java/com/example/demo/member/Member.java
+++ demo/src/main/java/com/example/demo/member/Member.java
@@ -10,18 +10,6 @@ public class Member {
     private String email;
     private LocalDateTime signupTime;
 
-    public Member() {
-    }
-
-    public Member(String id, String pw, String name, int age, String email, LocalDateTime signupTime) {
-        this.id = id;
-        this.pw = pw;
-        this.name = name;
-        this.age = age;
-        this.email = email;
-        this.signupTime = signupTime;
-    }
-
     public String getId() {
         return id;
     }
@@ -82,60 +70,3 @@ public class Member {
     // '}';
     // }
 }
-
-// public class Member {
-// private String id;
-// private String passwd;
-// private String name;
-// private int age;
-// private String email;
-// private LocalDateTime signuptime;
-
-// public String getId() {
-// return id;
-// }
-
-// public void setId(String id) {
-// this.id = id;
-// }
-
-// public String getPasswd() {
-// return passwd;
-// }
-
-// public void setPasswd(String passwd) {
-// this.passwd = passwd;
-// }
-
-// public String getName() {
-// return name;
-// }
-
-// public void setName(String name) {
-// this.name = name;
-// }
-
-// public int getAge() {
-// return age;
-// }
-
-// public void setAge(int age) {
-// this.age = age;
-// }
-
-// public String getEmail() {
-// return email;
-// }
-
-// public void setEmail(String email) {
-// this.email = email;
-// }
-
-// public LocalDateTime getSignuptime() {
-// return signuptime;
-// }
-
-// public void setSignuptime(LocalDateTime signuptime) {
-// this.signuptime = signuptime;
-// }
-// }
